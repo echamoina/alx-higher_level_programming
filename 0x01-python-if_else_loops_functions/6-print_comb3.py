fde1f34654a686ccd241f15154a12e3ebadf93ee
echamoina <ehamoinaecha@gmail.com>
2023-06-28 14:23:20 -0700

    0x01.Tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+for number in range(0, 10):
+    for number2 in range(0, 10):
+        if number2 <= number:
+            continue
+        elif number == 8 and number2 == 9:
+            print("{:d}{:d}".format(number, number2))
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print("{:d}{:d}".format(number, number2), end=', ')
